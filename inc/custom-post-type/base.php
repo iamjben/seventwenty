7a05fe20202eeda8911eee42506d8f50d15ec056
JB Balmes <john.balmes@randemgroup.com>
2020-01-20 18:13:03 +0800

    docs: updated comment on base.hp

--- inc/custom-post-type/base.php
+++ inc/custom-post-type/base.php
@@ -1,7 +1,8 @@
 <?php
-
+/**
+ * Modify the search query with posts_where
+ */
 function cpt_search_join($join) {
-	//  Modify the search query with posts_where
 	global $wpdb;
 	if (is_search()) {
 		$join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
@@ -10,8 +11,10 @@ function cpt_search_join($join) {
 }
 add_filter('posts_join', 'cpt_search_join');
 
+/**
+ * Prevent duplicates
+ */
 function cpt_search_distinct($where) {
-	// Prevent duplicates
 	global $wpdb;
 	if (is_search()) {
 		return "DISTINCT";
@@ -20,8 +23,10 @@ function cpt_search_distinct($where) {
 }
 add_filter('posts_distinct', 'cpt_search_distinct');
 
+/**
+ * FIX - Solves issue with /page/2/ of taxonomy giving a 404 error
+ */
 function cpt_taxonomy_request($query_string) {
-	// FIX - Solves issue with /page/2/ of taxonomy giving a 404 error
 	if (isset( $query_string['page'])) {
 		if ('' != $query_string['page']) {
 			if (isset( $query_string['name'])) {
@@ -33,16 +38,20 @@ function cpt_taxonomy_request($query_string) {
 }
 add_filter('request', 'cpt_taxonomy_request');
 
+/**
+ * Taxonomy Pre-get posts
+ */
 function cpt_taxonomy_get_posts($query) {
-	// Taxonomy Pre-get posts
 	if (is_tax() && $query->is_main_query() && !$query->is_feed() && !is_admin()) {
 		$query->set( 'paged', str_replace( '/', '', get_query_var( 'page' ) ) );
 	}
 }
 add_action('pre_get_posts', 'cpt_taxonomy_get_posts');
 
+/**
+ * Fix Permalink - Makes permalink work
+ */
 function cpt_flush_rewrite() {
-	// Fix Permalink - Makes permalink work
 	flush_rewrite_rules();
 }
 add_action('init', 'cpt_flush_rewrite');
